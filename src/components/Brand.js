987b9d4963d71b8f9f22d20bdcafd79e23d13009
bhavinpanchani <bhavinpanchani9@gmail.com>
2022-03-31 01:06:41 -0400

    testing call to api

--- src/components/Brand.js
+++ src/components/Brand.js
@@ -1,12 +1,46 @@
+import React, { Component } from 'react';
 import '../css/Brand.css';
+import axios from 'axios';
 
-function Brand() {
-  return (
-    <div className="Brand">
-      <h1>Bhavin Panchani</h1>
-      <h3>Full Stack Developer</h3>
-    </div>
-  );
+const api = axios.create({
+  // baseURL: `http://study-on.us-east-1.elasticbeanstalk.com/`
+  baseURL: `http://127.0.0.1:8080/`
+})
+
+// function Brand() {
+//   return (
+//     <div className="Brand">
+//       <div className='brand-content'>
+//         <h1 className='name'>BHAVIN PANCHANI</h1>
+//         <h3 className='occupation'>Full Stack Developer</h3>
+//       </div>
+//     </div>
+//   );
+// }
+
+class Brand extends Component {
+
+  constructor() {
+    super()
+    api.get('university/',
+      // {
+      //   mode: 'no-cors'
+      // }
+    ).then(res => {
+      console.log(res.data)
+    })
+  }
+
+  render() {
+    return (
+      <div className="Brand">
+        <div className='brand-content'>
+          <h1 className='name'>BHAVIN PANCHANI</h1>
+          <h3 className='occupation'>Full Stack Developer</h3>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Brand;
